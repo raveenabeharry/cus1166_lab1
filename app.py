902ca94146d2342c86bbc077d859c15a02298262
Raveena Beharry <raveena.beharry16@stjohns.edu>
2019-02-07 14:05:06 -0500

    contains the main class

---
+++ app.py
@@ -0,0 +1,18 @@
+from mymodules import average_grade
+from mymodules import Student
+
+def main():
+
+    student1=Student("Alyssa Milano", 87 )
+    student2=Student("Samantha Perez", 77 )
+    student11=Student("Raveena Beharry", 99 )
+    student11=Student("Mona Balkaran", 65 )
+    student11=Student("Jason Persaud", 83 )
+    student11=Student("Michael Milano", 97 )
+    student11=Student("Matthew Smith", 76 )
+    student11=Student("Jessica Medna", 88 )
+    student11=Student("Maria Shriver", 89 )
+    student11=Student("Karen Gill", 67 )
+
+    print_student_info()
+    print(average_grade)
